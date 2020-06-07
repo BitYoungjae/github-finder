a10f73cb29fd161488d56a32cbeda38a4741fee5
BitYoungjae <bityoungjae@gmail.com>
2020-06-08 03:22:53 +0900

    chore(app.js): 가상의 user prop 으로 UserItem 그려보기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
+        <UserItem
+          user={{
+            id: 'id',
+            login: 'mojombo',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+            html_url: 'http://bityoungjae.com',
+          }}
+        />
       </div>
     );
   }
