890545dcb2c19b84fb805aed0a2a082b3750924d
BitYoungjae <bityoungjae@gmail.com>
2020-06-08 04:10:55 +0900

    feat(fakeuserhelper.js): 재사용 가능하도록 class 로 변경
    BREAKING CHANGE: 사용법이 달라짐. 함수를 즉시 호출하지 않고 인스터스화 필수

--- src/utils/fakeUserHelper.js
+++ src/utils/fakeUserHelper.js
@@ -1,32 +1,35 @@
 const chance = require('chance').Chance();
-let userId = 1;
 
-export function makeAvatarUrl(userNumber) {
-  return `https://avatars0.githubusercontent.com/u/${userNumber}?v=4`;
-}
+export class FakeUserCreator {
+  userId = 1;
 
-export function makeFakeUser(userName) {
-  return {
-    id: userId,
-    login: userName,
-    avata_url: makeAvatarUrl(userId++),
-    html_url: `https://github.com/${userName.replace(' ', '')}`,
-  };
-}
+  makeAvatarUrl(userNumber) {
+    return `https://avatars0.githubusercontent.com/u/${userNumber}?v=4`;
+  }
 
-export function makeFakeUsersFromArray(...userArr) {
-  return userArr.map((userName) => makeFakeUser(userName));
-}
+  makeFakeUser(userName) {
+    return {
+      id: this.userId,
+      login: userName,
+      avata_url: this.makeAvatarUrl(this.userId++),
+      html_url: `https://github.com/${userName.replace(' ', '')}`,
+    };
+  }
 
-export function makeFakeUsers(howMany = 1) {
-  return makeFakeUsersFromArray(
-    ...Array(howMany)
-      .fill(null)
-      .map((_) =>
-        chance.name({
-          nationality: 'en',
-          gender: Math.random() > 0.5 ? 'male' : 'female',
-        }),
-      ),
-  );
+  makeFakeUsersFromArray(...userArr) {
+    return userArr.map((userName) => this.makeFakeUser(userName));
+  }
+
+  makeFakeUsers(howMany = 1) {
+    return this.makeFakeUsersFromArray(
+      ...Array(howMany)
+        .fill(null)
+        .map((_) =>
+          chance.name({
+            nationality: 'en',
+            gender: Math.random() > 0.5 ? 'male' : 'female',
+          }),
+        ),
+    );
+  }
 }
