d91b3ddc999ba76250f975a727231ee514bbdab0
BitYoungjae <bityoungjae@gmail.com>
2020-06-08 03:31:19 +0900

    refactor(navbar.js): props에 비구조화 할당 적용

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -13,10 +13,12 @@ export class Navbar extends Component {
   };
 
   render() {
+    const { icon, title } = this.props;
+
     return (
       <nav className='navbar bg-primary'>
         <h1>
-          <i className={this.props.icon}></i> {this.props.title}
+          <i className={icon}></i> {title}
         </h1>
       </nav>
     );
