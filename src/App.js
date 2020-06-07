c9243b2c5cd70fa11d23509841a9accbdf8473eb
BitYoungjae <bityoungjae@gmail.com>
2020-06-08 04:44:34 +0900

    chore(app.js): 데모 렌더링 위해 Users에 fakeUsers 사용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
-        <UserItem
-          user={{
-            id: 'id',
-            login: 'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'http://bityoungjae.com',
-          }}
-        />
+        <Users numOfFakeUsers={50} />
       </div>
     );
   }
